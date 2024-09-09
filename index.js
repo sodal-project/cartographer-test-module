df6f5410777c04d3daebbacbe314587c48fcb811
Tom Benbow <tom@hellomoon.net>
2024-09-09 15:33:53 -0500

    Add node slack sdk dependency

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
+const { WebClient } = require('@slack/web-api');
+
 console.log("Logging from the test module");
+
+if (WebClient) {
+  console.log("WebClient is available");
+  console.log(WebClient);
+} else {
+  console.log("No WebClient found");
+}
