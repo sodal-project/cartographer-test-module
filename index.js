7ae61ee19998bc2fa38cc9f41152f3ae72134c73
Tom Benbow <tom@hellomoon.net>
2024-09-09 16:50:29 -0500

    Simplify test module

--- index.js
+++ index.js
@@ -1,10 +1,5 @@
 const { WebClient } = require('@slack/web-api');
 
-// Check if the module is being executed as a child process
-if (process.send) {
-  process.send("Logging from the test module");
-}
-
 if (WebClient) {
   process.send("Slack WebClient is available");
 } else {
